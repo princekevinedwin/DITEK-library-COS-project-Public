79abe7a1ae016f473021fbb52387ba4fe3e98194
tanko dinshiya mai nasara <maiyasasa.tankodinshiya@binghamuni.edu.ng>
2024-08-05 20:51:38 +0100

    add auth page routes

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,11 @@ from app import app
 @app.route("/")
 def home_page():
     return render_template("MainPage.html")
+
+@app.route("/reg")
+def reg_page():
+    return render_template("reg.html")
+
+@app.route("/sign")
+def sign_page():
+    return render_template("sign.html")
