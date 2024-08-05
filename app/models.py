401df8981ff4eeb48da0cfbf86e1d3c7ec6bcd73
davidcodes <aiyejinadavid5@gmail.com>
2024-08-05 22:24:18 +0100

    add login andf register route

--- app/models.py
+++ app/models.py
@@ -2,5 +2,5 @@ from app import db
 
 class User(db.Model):
     id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(50), required=True, nullable=False)
+    username=db.Column(db.String(50), required=True, nullable=False, unique=True)
     password=db.Column(db.String(256))
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
-from flask import render_template
-from app import app
+from flask import render_template, request
+from app import app, db
+from flask_login import login_user, current_user
+from models import User
 
 @app.route("/")
 def home_page():
-    return render_template("MainPage.html")
+    return render_template("MainPage.html", current_user=current_user)
 
 @app.route("/reg")
 def reg_page():
@@ -12,3 +14,22 @@ def reg_page():
 @app.route("/sign")
 def sign_page():
     return render_template("sign.html")
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("username")
+    user = db.session.query(User).get(username=username)
+    login_user(user)
+    return "User Logged In", 200
+
+@app.route("/api/register", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("username")
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return "User Created successfully", 201
