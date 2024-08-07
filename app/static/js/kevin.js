9d736fc32c16cad82d24f71147ef12fccff8433b
prince kevin <princekevinedwin63@gmail.com>
2024-08-07 15:13:04 +0100

    new arrivals and java amend

--- app/static/js/kevin.js
+++ app/static/js/kevin.js
@@ -112,6 +112,27 @@ var swiperFeatured = new Swiper(".featured-slider", {
 });
 
 
+var swiperFeatured = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
 
 
 var swiperFeatured = new Swiper(".reviews-slider", {
